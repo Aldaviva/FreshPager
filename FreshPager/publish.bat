5ba92e212253b2137d719cce509fff63df588109
Ben Hutchison <ben@aldaviva.com>
2025-03-25 23:08:32 -0700

    Update to a newer version of PagerDuty that can handle webhooks where the agent type is inbound_integration_reference

--- FreshPager/publish.bat
+++ FreshPager/publish.bat
@@ -2,4 +2,4 @@
 
 dotnet restore
 dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true --self-contained false --no-restore
-dotnet publish -c Release -r linux-arm -p:PublishSingleFile=true --self-contained false --no-restore
+dotnet publish -c Release -r linux-arm64 -p:PublishSingleFile=true --self-contained false --no-restore
